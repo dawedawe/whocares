2796bfc95a8ff603454b9e481a9c1d8f739175ff
dawe <dawedawe@posteo.de>
2024-06-23 17:28:17 +0200

    refactor to use struct

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,14 @@ struct Config {
     caretakers: Vec<String>,
 }
 
-fn get_schedule(path: &str) -> io::Result<Config> {
+struct CareWeek {
+    week: u32,
+    caretaker: String,
+    start_date: chrono::NaiveDate,
+    end_date: chrono::NaiveDate,
+}
+
+fn get_config(path: &str) -> io::Result<Config> {
     if let Ok(file) = File::open(path) {
         let reader = io::BufReader::new(file);
         let schedule: Config = serde_json::from_reader(reader).unwrap();
@@ -33,28 +40,37 @@ fn get_current_caretaker(schedule_start_week: u32, caretakers: &Vec<String>) -> 
     caretakers.get(caretaker_idx).unwrap().to_string()
 }
 
-fn get_next_weeks_schedule(schedule_start_week: u32, caretakers: &Vec<String>) -> Vec<String> {
-    let len = caretakers.len();
-    let caretaker_idx = get_current_caretaker_idx(schedule_start_week, len as u32);
-    let mut weeks = Vec::new();
-    for i in caretaker_idx..(caretaker_idx + len) {
+fn get_next_weeks(conf: Config, weeks: u32) -> Vec<CareWeek> {
+    let len = conf.caretakers.len();
+    let caretaker_idx = get_current_caretaker_idx(conf.startweek, len as u32);
+    let mut caretaker_ordered = Vec::new();
+    for i in caretaker_idx..(caretaker_idx + weeks as usize) {
         let idx = i % len;
-        weeks.push(caretakers.get(idx).unwrap());
+        caretaker_ordered.push(conf.caretakers.get(idx).unwrap());
     }
     let current_week_number = chrono::Local::now().iso_week().week();
-    weeks
+    caretaker_ordered
         .iter()
         .zip(current_week_number..)
-        .map(|(caretaker, week)| format!("Week {}: {}", week, caretaker))
-        .collect::<Vec<String>>()
+        .map(|(caretaker, week)| {
+            let start = chrono::NaiveDate::from_isoywd_opt(chrono::Local::now().year(), week, chrono::Weekday::Mon).unwrap();
+            let end = chrono::NaiveDate::from_isoywd_opt(chrono::Local::now().year(), week, chrono::Weekday::Sun).unwrap();
+            CareWeek {
+                week: week,
+                caretaker: caretaker.to_string(),
+                start_date: start,
+                end_date: end,
+            }
+        })
+        .collect::<Vec<CareWeek>>()
 }
 
 fn main() {
-    match get_schedule(PATH) {
-        Ok(schedule) => {
-            let weeks = get_next_weeks_schedule(schedule.startweek, &schedule.caretakers);
+    match get_config(PATH) {
+        Ok(conf) => {
+            let weeks = get_next_weeks(conf, 8);
             for week in weeks {
-                println!("{}", week);
+                println!("week #{} {} - {}: {}", week.week, week.start_date, week.end_date, week.caretaker);
             }
         }
         _ => panic!("Failed to open file"),
@@ -67,13 +83,13 @@ mod tests {
 
     #[test]
     fn deserialization_works() {
-        let result = get_schedule(PATH);
+        let result = get_config(PATH);
         assert!(result.is_ok());
     }
 
     #[test]
     fn get_current_caretaker_works() {
-        let schedule = get_schedule(PATH).unwrap();
+        let schedule = get_config(PATH).unwrap();
         let current_caretaker = get_current_caretaker(schedule.startweek, &schedule.caretakers);
         assert!(schedule.caretakers.contains(&current_caretaker));
     }
